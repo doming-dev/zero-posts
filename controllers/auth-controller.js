7197cc609976df9c580547e1f8153726b6a26de4
Dom <domingo-dev@hotmail.com>
2019-09-28 20:22:55 -0700

    added login / signup code with pw encryption / decryption

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -2,24 +2,73 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const User = require('../models/user');
 
-// TODO: continue with sign up logic
-
 exports.getLogin = (req, res, next) => {
-    res.render('login');
+    res.render('login', { email: '', error: '' });
 };
 
-exports.postLogin = (req, res, next) => {
+exports.postLogin = async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
+    const user = await User.findOne();
+
+    if (user) {
+        const passwordMatches = await bcrypt.compare(password, user.password);
+        if (passwordMatches === true) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            res.redirect('/');
+        } else {
+            res.render('login', {
+                email: email,
+                error: 'Email or Password is incorrect'
+            });
+        }
+    } else {
+        res.render('login', {
+            email: email,
+            error: 'Email or Password is incorrect'
+        });
+    }
 };
 
 exports.getSignup = (req, res, next) => {
-    res.render('signup');
+    res.render('signup', { email: '', error: '' });
 };
 
-exports.postSignup = (req, res, next) => {
+exports.postSignup = async (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
 
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser != null) {
+        res.render('signup', {
+            email: email,
+            error: 'This email is already registered'
+        });
+        return;
+    }
+    if (password === confirmPassword) {
+        var hashedPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({
+            email: email,
+            password: hashedPassword,
+            posts: [],
+            likedPosts: [],
+            dislikedPosts: []
+        });
 
+        await newUser.save();
+
+        res.render('login', {
+            email: email,
+            error: ''
+        });
+    } else {
+        res.render('signup', {
+            email: email,
+            error: 'Passwords do not match!'
+        });
+    }
 };
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -5,6 +5,7 @@ const Router = express.Router();
 
 Router.get('/login', authController.getLogin);
 Router.get('/signup', authController.getSignup);
-
+Router.post('/signup', authController.postSignup);
+Router.post('/login', authController.postLogin);
 
 module.exports = Router;
